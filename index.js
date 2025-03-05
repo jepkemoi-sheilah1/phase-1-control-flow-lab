bb243a9a0470c14d730b7c3f85aaa266c5c4172f
Jepkemoi Sheilah <jepkemoishyllah@gmail.com>
2025-03-05 23:14:58 +0300

    modified the javascript file using the control flow statements and completed

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet( distance){//block of code
   // Write your code here!
+  if(distance<=400){//if the feet is less or equal to 400
+    return 'This one is on me!';//return this string
+  }
+  else if (distance>400 && distance <=2000){//if the feet is equal to 400 and less than 2000
+    return 'That will be twenty bucks.';//return this string
+  } 
+else if (distance >2000 && distance <=2500){//if the feet is grater than 2500
+  return 'I will gladly take your thirty bucks.'; //return this string
+}
+else{
+  return 'No can do.';//return this string
+}
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){//block of code
   // Write your code here!
+  return city==='NYC' ? 'Ok, sounds good.':'No go.';//using the ternary operator
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip( tip){// block code
+
   // Write your code here!
+  switch(tip){// using the switch statement
+    case 'generous':// if the tip is generous
+    return "Thank you so much."; //return this string
+    case 'not as generous'://
+     return "Thank you.";//retun this string
+    default:
+      return "Bye.";//return this string 
+  }
 }
+  
